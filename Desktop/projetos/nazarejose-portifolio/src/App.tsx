7373ed89127a801bf23940a463473e72ed92518c
nazarejose <josenazare1234@gmail.com>
2024-03-04 18:11:26 -0300

    navbar and part of the header

---
+++ Desktop/projetos/nazarejose-portifolio/src/App.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "./components/Navbar"
+import { Header } from "./components/Header"
+import { Blob } from "./components/Blob"
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Header />
+    <Blob />
+
+    </>
+  )
+}
+
+export default App
