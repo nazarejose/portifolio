078e8fdf113b44d1c59f5effa0a924b6fb36ffbd
nazarejose <josenazare1234@gmail.com>
2024-06-13 20:20:00 -0300

    component removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Navbar } from "./components/Navbar"
 import { Header } from "./components/Header"
-import { Blob } from "./components/Blob"
 
 function App() {
   return (
     <>
     <Navbar/>
     <Header />
-    <Blob />
-
     </>
   )
 }
